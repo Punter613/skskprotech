3c6882f49a6169befb8f563af039fbdead4755e4
punter613 <bshaffer613@gmail.com>
2026-06-14 12:18:48 -0400

    Update services and routes

---
+++ src/routes/payments.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+router.post('/charge', async (req, res, next) => {
+  try {
+    const { amount } = req.body;
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: Math.round(Number(amount || 0) * 100),
+      currency: 'usd'
+    });
+
+    res.json({
+      success: true,
+      clientSecret: paymentIntent.client_secret
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
