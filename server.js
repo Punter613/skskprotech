76edda987f45cf1c86e1e4c1cc00a2bdbf4d9b6e
Punter613 <30910761+Punter613@users.noreply.github.com>
2026-06-27 00:08:46 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
 const { createClient } = require('@supabase/supabase-js');
 const { z } = require('zod');
 
