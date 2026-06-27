2ad2811ee76f665d8127920f2619bc92b446a199
Punter613 <30910761+Punter613@users.noreply.github.com>
2026-06-27 00:20:45 -0700

    Update server.js

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ const jobsRouter = require('./src/routes/jobs');
 app.use('/api/jobs', jobsRouter);
 app.use('/api/diagnose', diagnose);
 app.use('/api/estimateHeuristic', verifyToken, estimateHeuristic); 
-app.use('/api/invoice', invoice);
+app.use('/src/api/invoice', invoice);
 app.use('/api/translate', require('./src/routes/translate'));
 app.use(oemRouter);
 
